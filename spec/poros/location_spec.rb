9c013a5e9c3c122683df8f7423a1e6a76b8eadfe
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-06-10 13:56:01 -0600

    Test: add test for location poro

---
+++ spec/poros/location_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Location do
+
+  it 'exists and has readable attributes' do
+    data = File.read('/spec/poros/fixures/location.txt')
+    binding.pry
+
+
+  end 
+
+
+
+
+end 
