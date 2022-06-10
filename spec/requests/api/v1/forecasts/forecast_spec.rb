9e10e46899cf0c95d2c9f45dca5d0d4225011c02
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-06-10 14:47:44 -0600

    Test: add test for first request, get forecast for given city

--- spec/requests/api/v1/forecasts/forecast_spec.rb
+++ spec/requests/api/v1/forecasts/forecast_spec.rb
@@ -2,10 +2,18 @@ require 'rails_helper'
 
 RSpec.describe 'Get Forecast Endpoint' do
 
-  it 'returns the forcast for a given city' do
+  it 'returns serialized JSON forcast for a given city' do
+    headers = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json' }
+    city = 'boston,ma'
 
+    get '/api/v1/forecast', headers: headers params: { location: city }
+    
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
 
+    forecast = JSON.parse(response.body, symbolize_names: true)
 
+    expect(forecast).to be_a Hash
   end 
 
 
