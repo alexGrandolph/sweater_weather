9f859bef468a857b8a106217687e231d20b401dc
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-06-10 16:31:11 -0600

    Feat: add current weather poro

---
+++ app/poros/current_weather.rb
@@ -0,0 +1,19 @@
+class CurrentWeather
+  attr_reader :datetime, :sunrise, :sunset, :temp, :feels_like, :humidity, :uvi, :visibility, :conditions, :icon
+
+
+  def initialize(attributes)
+    @datetime = Time.at(attributes[:dt])
+    @sunrise = Time.at(attributes[:sunrise])
+    @sunset = Time.at(attributes[:sunset])
+    @temp = attributes[:temp]
+    @feels_like = attributes[:feels_like]
+    @humidity = attributes[:humidity]
+    @uvi = attributes[:uvi]
+    @visibility = attributes[:visibility]
+    @conditions = attributes[:weather][0][:main]
+    @icon = attributes[:weather][0][:icon]
+  end 
+
+
+end 
--- spec/poros/current_weather_spec.rb
+++ spec/poros/current_weather_spec.rb
@@ -3,30 +3,30 @@ require 'rails_helper'
 RSpec.describe CurrentWeather do
 
   it 'exists and has readable attributes' do
-    data = "current": {
-                        "dt": 1654893027,
-                        "sunrise": 1654852037,
-                        "sunset": 1654906818,
-                        "temp": 75.43,
-                        "feels_like": 74.61,
-                        # "pressure": 1008,
-                        "humidity": 42,
-                        # "dew_point": 284.35,
-                        "uvi": 2.18,
-                        # "clouds": 40,
-                        "visibility": 10000,
-                        "wind_speed": 5.66,
-                        "wind_deg": 270,
-                        "wind_gust": 10.29,
-                        "weather": [
-                                    {
-                                        "id": 802,
-                                        "main": "Clouds",
-                                        "description": "scattered clouds",
-                                        "icon": "03d"
-                                    }
-                                    ]
-                      }
+    data = {
+              "dt": 1654893027,
+              "sunrise": 1654852037,
+              "sunset": 1654906818,
+              "temp": 75.43,
+              "feels_like": 74.61,
+              # "pressure": 1008,
+              "humidity": 42,
+              # "dew_point": 284.35,
+              "uvi": 2.18,
+              # "clouds": 40,
+              "visibility": 10000,
+              "wind_speed": 5.66,
+              "wind_deg": 270,
+              "wind_gust": 10.29,
+              "weather": [
+                          {
+                              "id": 802,
+                              "main": "Clouds",
+                              "description": "scattered clouds",
+                              "icon": "03d"
+                          }
+                          ]
+            }
     current = CurrentWeather.new(data)
     expect(current).to be_a CurrentWeather
     expect(current.datetime).to eq('2022-06-10 14:30:27 -0600')
@@ -38,7 +38,7 @@ RSpec.describe CurrentWeather do
     expect(current.uvi).to eq(2.18)
     expect(current.visibility).to eq(10000)
     expect(current.conditions).to eq("Clouds")
-    expect(current.icon).to eq("0.d")
+    expect(current.icon).to eq("03d")
   
   end 
 
