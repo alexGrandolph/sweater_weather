ee3a016d01d8ac0ac22b828a2f102e32662dcc07
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-06-10 14:50:37 -0600

    Feat: add namespaced route and controller for forecast#index

---
+++ app/controllers/api/v1/forecast_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::ForecastController < ApplicationController
+
+  def index
+
+  end 
+
+
+end 
--- spec/requests/api/v1/forecasts/forecast_spec.rb
+++ spec/requests/api/v1/forecasts/forecast_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe 'Get Forecast Endpoint' do
 
-  it 'returns serialized JSON forcast for a given city' do
+  it 'returns serialized JSON forcast for a given city', :vcr do
     headers = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json' }
     city = 'boston,ma'
 
-    get '/api/v1/forecast', headers: headers params: { location: city }
+    get '/api/v1/forecast', headers: headers, params: { location: city }
     
     expect(response).to be_successful
     expect(response.status).to eq(200)
