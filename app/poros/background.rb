543eb06b668893888bfd0ae5b5c5e0263b6a6d6d
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-06-11 14:30:16 -0600

    Feat: add background poro

--- app/poros/background.rb
+++ app/poros/background.rb
@@ -1,10 +1,12 @@
 class Background
-
+  attr_reader :description, :image_url, :source, :author, :area
 
   def initialize(attributes)
-
-
+    @description = attributes[:description]
+    @image_url = attributes[:urls][:full]
+    @source = attributes[:links][:self]
+    @author = attributes[:user][:name]
+    @area = attributes[:tags][0][:title]
   end 
-
 
 end
--- spec/poros/background_spec.rb
+++ spec/poros/background_spec.rb
@@ -2,16 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Background do
   it 'exists and has readable attributes', :vcr do
-    data = BackgroundService.get_background_for_location('miami,fl')[:results]
+    data = BackgroundService.get_background_for_location('miami,fl')[:results][0]
     # binding.pry
 
 
     background = Background.new(data)
 
     expect(background).to be_a Background
-    expect(background.location).to eq('Miami, FL')
-    expect(background.image_url).to eq("https://images.unsplash.com/photo-1642980364256-12e2811c91ae?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMzY4Njd8MHwxfHNlYXJjaHwxfHxtaWFtaSUyQ2ZsfGVufDB8fHx8MTY1NDk3ODQ1Ng&ixlib=rb-1.2.1&q=80")
-    expect(background.source).to eq('unsplash.com')
+    expect(background.image_url).to eq("https://images.unsplash.com/photo-1642980364256-12e2811c91ae?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMzY4Njd8MHwxfHNlYXJjaHwxfHxtaWFtaSUyQ2ZsfGVufDB8fHx8MTY1NDk3NDI2NA&ixlib=rb-1.2.1&q=80")
+    expect(background.source).to eq('https://api.unsplash.com/photos/67uJo7mZPCo')
     expect(background.author).to eq('Andre Tan')
+    expect(background.description).to eq("instagram: @andredantan")
+    expect(background.area).to eq("miami beach")
+
   end
 end
