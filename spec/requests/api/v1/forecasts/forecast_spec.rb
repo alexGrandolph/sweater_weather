d5a61d8882fecf1e7a8ae74e48809b7b99191a7a
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-06-11 11:00:59 -0600

    Test: add expectations for current weather response test, split current, daily, hourly weather to be individual tests, cuts back on length of test

--- spec/requests/api/v1/forecasts/forecast_spec.rb
+++ spec/requests/api/v1/forecasts/forecast_spec.rb
@@ -1,28 +1,63 @@
 require 'rails_helper'
 
 RSpec.describe 'Get Forecast Endpoint' do
+  describe 'Current Weather In Response Test'
+    it 'Current weather attribute is present with only desired keys/values', :vcr do
+      headers = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json' }
+      city = 'boston,ma'
 
-  it 'returns serialized JSON forcast for a given city', :vcr do
-    headers = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json' }
-    city = 'boston,ma'
+      get '/api/v1/forecast', headers: headers, params: { location: city }
+      
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
 
-    get '/api/v1/forecast', headers: headers, params: { location: city }
-    
-    expect(response).to be_successful
-    expect(response.status).to eq(200)
+      raw_forecast = JSON.parse(response.body, symbolize_names: true)
+      expect(raw_forecast).to have_key(:data)
+      
+      forecast = raw_forecast[:data]
+      
+      expect(forecast).to be_a Hash
+      expect(forecast).to have_key(:id)
+      expect(forecast[:type]).to eq('forecast')
+      expect(forecast[:attributes]).to be_a Hash
 
-    raw_forecast = JSON.parse(response.body, symbolize_names: true)
-    expect(raw_forecast).to have_key(:data)
-    
-    forecast = raw_forecast[:data]
-    
-    expect(forecast).to be_a Hash
-    expect(forecast).to have_key(:id)
-    expect(forecast[:type]).to eq('forecast')
+      attributes = forecast[:attributes]
+      expect(attributes).to have_key(:current_weather)
+      expect(attributes[:current_weather]).to be_a Hash
+      expect(attributes[:current_weather]).to have_key(:datetime)
+      expect(attributes[:current_weather]).to have_key(:sunrise)
+      expect(attributes[:current_weather]).to have_key(:sunset)
+      expect(attributes[:current_weather]).to have_key(:feelsLike)
+      expect(attributes[:current_weather]).to have_key(:humidity)
+      expect(attributes[:current_weather]).to have_key(:uvi)
+      expect(attributes[:current_weather]).to have_key(:visibility)
+      expect(attributes[:current_weather]).to have_key(:conditions)
+      expect(attributes[:current_weather]).to have_key(:icon)
+
+      expect(attributes[:current_weather]).to_not have_key(:clouds)
+      expect(attributes[:current_weather]).to_not have_key(:windspeed)
+      expect(attributes[:current_weather]).to_not have_key(:pressure)
+      expect(attributes[:current_weather]).to_not have_key(:dewpoint)
+      expect(attributes[:current_weather]).to_not have_key(:wind_deg)
+      expect(attributes[:current_weather]).to_not have_key(:wind_gust)
+      expect(attributes[:current_weather]).to_not have_key(:weather)
+    end 
   end 
 
+  describe 'Daily Weather In Response Test' do
 
+    it 'Daily weather attribute is present with only desired keys/values', :vcr do
+      headers = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json' }
+      city = 'san francisco,ca'
 
+      get '/api/v1/forecast', headers: headers, params: { location: city }
 
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
 
+      raw_forecast = JSON.parse(response.body, symbolize_names: true)
+
+      binding.pry
+
+  end 
 end 
