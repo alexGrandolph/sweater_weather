31e9f9f564cc00984333c54154b0e5d4e8ad8c8d
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-06-12 16:06:16 -0600

    Feat: add create user functionality in users controller, include creation of api key, calls to users serializer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,9 +2,20 @@ class Api::V1::UsersController < ApplicationController
 
 
   def create
-    binding.pry
+    
+    if params[:password] == params[:password_confirmation]
+      user = User.new(email: params[:email], password: params[:password])
+      if user.save
+        user.api_key = SecureRandom.hex 
+      end
+      render json: UserSerializer.new_user(user)
+    else 
+      render json: status: 404
+    end 
   end 
 
+  private
+  
 
 
 end 
