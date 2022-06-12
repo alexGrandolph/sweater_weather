37299b2fd8c1c14b150620e2c0ff63ca1f442a42
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-06-11 18:39:48 -0600

    Test: add test for password being encrypted for a user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,5 +6,10 @@ RSpec.describe User, type: :model do
     it { should validate_uniqueness_of(:email) }
     it { should validate_presence_of(:password_digest)}
 
+    it 'encrpyts a users password' do
+      user = User.create(email: 'dog@test.com', password: 'password123', password_confirmation: 'password123')
+      expect(user).to_not have_attribute(:password)
+      expect(user.password_digest).to_not eq('password123')
+    end 
   end 
 end
