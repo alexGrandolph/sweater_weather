509d3c5b5681e2d463a9a13c6ce7d018aec1e8b8
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-06-11 18:30:30 -0600

    Docs: add api_key_maker gem

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,10 @@
+class Api::V1::UsersController < ApplicationController
+
+
+  def create
+    binding.pry
+  end 
+
+
+
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
       get '/forecast', to: 'forecast#index'
       get '/backgrounds', to: 'backgrounds#index'
 
+      post '/users', to: 'users#create'
+
 
     end 
   end 
