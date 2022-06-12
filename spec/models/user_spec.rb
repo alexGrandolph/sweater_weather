6138e9742755550475eb663625ca9118ecacf64b
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-06-12 15:53:39 -0600

    test: add validation test for uniqueness of api_key

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email) }
+    # it { should validate_presence_of(:api_key) }
+    it { should validate_uniqueness_of(:api_key) }
     it { should validate_presence_of(:password_digest)}
 
     it 'encrpyts a users password' do
@@ -11,5 +13,7 @@ RSpec.describe User, type: :model do
       expect(user).to_not have_attribute(:password)
       expect(user.password_digest).to_not eq('password123')
     end 
+
+    
   end 
 end
