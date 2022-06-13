e1ee8ff113e443092f334bf0c7646440beece901
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-06-13 10:49:38 -0600

    Refactor: update book facade spec to test the amount of book objects returned matching total books found value that is also returned in the get_the_books method call

--- spec/facades/book_facade_spec.rb
+++ spec/facades/book_facade_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 RSpec.describe BookFacade do
 
-  it 'returns a Book poro', :vcr do
-    book = BookFacade.get_the_books('denver,co', 3)
-
-    expect(book[0]).to be_all Book
-    expect(book[0].count).to eq(3)
+  it 'returns a Book poro and the total number of books matching the search term', :vcr do
+    books = BookFacade.get_the_books('denver,co', 4)
+   
+    expect(books[0]).to be_all Book
+    expect(books[1]).to be_an Integer
+    expect(books[0].count).to eq(books[1])
   end
 
 
