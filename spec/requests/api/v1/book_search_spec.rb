09e3c95c1a14c0e45e1cf74c0a2aa18514b4f193
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-06-13 10:28:03 -0600

    Test: add sad path to get book-search endpoint for if negative quantity param is given

--- spec/requests/api/v1/book_search_spec.rb
+++ spec/requests/api/v1/book_search_spec.rb
@@ -1,44 +1,68 @@
 require 'rails_helper'
 
 RSpec.describe 'Get Book-Search Endpoint' do
-  it 'returns a JSON respones contaning books backing search parameters', :vcr do
-    headers = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json' }
-    city = 'boston,ma'
-    get '/api/v1/book-search', headers: headers, params: { location: city, quantity: 5 }
+  describe 'Happy Path' do
+    it 'returns a JSON respones contaning books backing search parameters', :vcr do
+      headers = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json' }
+      city = 'boston,ma'
+      get '/api/v1/book-search', headers: headers, params: { location: city, quantity: 5 }
 
-    expect(response).to be_successful
-    expect(response.status).to eq(200)
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
 
-    full_result = JSON.parse(response.body, symbolize_names: true)
-    expect(full_result).to have_key(:data)
+      full_result = JSON.parse(response.body, symbolize_names: true)
+      expect(full_result).to have_key(:data)
 
-    expect(full_result[:data]).to have_key(:id)
-    expect(full_result[:data]).to have_key(:type)
-    expect(full_result[:data]).to have_key(:attributes)
-   
-    data = full_result[:data][:attributes]
-
-    expect(data).to have_key(:destination)
+      expect(full_result[:data]).to have_key(:id)
+      expect(full_result[:data]).to have_key(:type)
+      expect(full_result[:data]).to have_key(:attributes)
     
-    expect(data).to have_key(:forecast)
-    expect(data[:forecast]).to be_a Hash
-    expect(data[:forecast]).to have_key(:summary)
-    expect(data[:forecast]).to have_key(:temperature)
-    
-    expect(data).to have_key(:total_books_found)
-    expect(data[:total_books_found]).to be_an Integer
+      data = full_result[:data][:attributes]
 
-    expect(data).to have_key(:books)
-    expect(data[:books]).to be_an Array
-    expect(data[:books].count).to eq(5)
+      expect(data).to have_key(:destination)
+      
+      expect(data).to have_key(:forecast)
+      expect(data[:forecast]).to be_a Hash
+      expect(data[:forecast]).to have_key(:summary)
+      expect(data[:forecast]).to have_key(:temperature)
+      
+      expect(data).to have_key(:total_books_found)
+      expect(data[:total_books_found]).to be_an Integer
 
-    data[:books].each do |book|
-      expect(book).to be_a Hash
-      expect(book).to have_key(:isbn)
-      expect(book).to have_key(:title)
-      expect(book).to have_key(:publisher)
-    
+      expect(data).to have_key(:books)
+      expect(data[:books]).to be_an Array
+      expect(data[:books].count).to eq(5)
+
+      data[:books].each do |book|
+        expect(book).to be_a Hash
+        expect(book).to have_key(:isbn)
+        expect(book).to have_key(:title)
+        expect(book).to have_key(:publisher)
+      end
     end
+  end 
+  describe 'Sad Path' do
+    it 'giving bad parameters results in error response', :vcr do
+
+      headers = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json' }
+      city = 'seattle,wa'
+      get '/api/v1/book-search', headers: headers, params: { location: city, quantity: -5 }
+
+
+      result = JSON.parse(response.body, symbolize_names: true)
+      
+      expect(result).to have_key(:data)
+      expect(result[:data]).to have_key(:error)
+
+      expect(result[:data][:error]).to have_key(:message)
+      expect(result[:data][:error][:message]).to eq("Quantity must be a positive integer")
+
+
+    end 
+
+
+
+
 
   end
 end
