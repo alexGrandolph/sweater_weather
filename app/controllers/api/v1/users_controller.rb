bcbe529cd9c36bab474778f75c69cd19311002d7
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-06-12 21:14:34 -0600

    Refactor: remove uniquness of api_key validation, was causing bugs on localhost/postman

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,11 +2,10 @@ class Api::V1::UsersController < ApplicationController
 
 
   def create
-    # binding.pry
     if params[:password] == params[:password_confirmation]
       user = User.create!(email: params[:email], password: params[:password])
       if user.save
-        user.api_key = SecureRandom.hex 
+        user.api_key = SecureRandom.hex(15)
       end
       render json: UserSerializer.new_user(user), status: 201
     else 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   validates_presence_of :email
   validates_uniqueness_of :email
   validates_presence_of :password_digest, required: true
-  validates_uniqueness_of :api_key
+  # validates_uniqueness_of :api_key
 
   has_secure_password
 
