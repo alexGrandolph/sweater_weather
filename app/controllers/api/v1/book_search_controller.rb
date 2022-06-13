32e13884f900135a858cf86d3c22363c3a88570c
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-06-13 09:52:48 -0600

    Feat: add calls to Location and Forecast facade to get weather data for book-search endpoint and in the #index action, need forecast data for seraizlier/response

--- app/controllers/api/v1/book_search_controller.rb
+++ app/controllers/api/v1/book_search_controller.rb
@@ -2,10 +2,13 @@ class Api::V1::BookSearchController < ApplicationController
 
 
   def index
-    location = params[:location]
+    city = params[:location]
     quantity = params[:quantity]
-    books = BookFacade.get_the_books(location, quantity)
-
+    books = BookFacade.get_the_books(city, quantity)
+    
+    location = LocationFacade.coordinates_for_city(city)
+    forecast = ForecastFacade.get_forecast(location.latitude, location.longitude)
+    binding.pry
   end 
 
 
