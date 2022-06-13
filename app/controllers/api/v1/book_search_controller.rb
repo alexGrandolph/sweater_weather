489a24cebf327e82ae28b7226e56afaccf04adec
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-06-13 09:25:44 -0600

    feat: add call to book facade in controller, development of end point will continue after development of facade poro service for books

--- app/controllers/api/v1/book_search_controller.rb
+++ app/controllers/api/v1/book_search_controller.rb
@@ -2,7 +2,9 @@ class Api::V1::BookSearchController < ApplicationController
 
 
   def index
-
+    location = params[:location]
+    quantity = params[:quantity]
+    books = BookFacade.get_the_books(location, quantity)
 
   end 
 
