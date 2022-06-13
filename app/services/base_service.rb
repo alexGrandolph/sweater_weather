c305300e90c5c880789f9041bca88e7ff88a81dd
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-06-13 11:36:00 -0600

    Feat: add BaseService for BookService, and other services to inherit self.conn method from

---
+++ app/services/base_service.rb
@@ -0,0 +1,8 @@
+class BaseService
+
+  def self.conn(url)
+    Faraday.new(url)
+  end
+
+
+end 
--- app/services/book_service.rb
+++ app/services/book_service.rb
@@ -1,8 +1,8 @@
-class BookService
+class BookService < BaseService
 
   def self.get_books(location, quantity)
-    conn = Faraday.new(url: "http://openlibrary.org/")
-    response = conn.get('search.json') do |c|
+    connection = conn("http://openlibrary.org/")
+    response = connection.get('search.json') do |c|
       c.params['place'] = location
       c.params['limit'] = quantity
     end
